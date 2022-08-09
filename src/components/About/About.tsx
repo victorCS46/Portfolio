f5274ca5d52d65f943211655a812738fe1a68542
Victor Cruz <vcruz@viva.com.do>
2022-08-09 16:53:36 -0400

    Working on skills terminal in about page

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Box } from '@mui/material';
-import { FiberManualRecord } from '@mui/icons-material';
+import { Box, Typography, useTheme } from '@mui/material';
+import Terminal from './Terminal';
 
 const About = (): JSX.Element => {
+
+  const { palette } = useTheme();
 
   return (
     <Box
@@ -11,29 +13,163 @@ const About = (): JSX.Element => {
       alignItems="center"
       borderRadius="1rem"
     >
-      <Box width={{ xs: '80%', md: '50%' }}>
+      {/* terminal style based on ZSH steef theme */}
+      <Terminal>
+        {/* header */}
+        <p>
+          <span style={{color: '#af5fff'}}>victorcruz</span> at 
+          <span style={{color: '#d75f00'}}> about</span> in 
+          <span style={{color: '#87fd03'}}> ~/portfolio</span> 
+          <span style={{color: '#56c2e6'}}> (main)</span><br/>
+          $ cat aboutMe.txt
+        </p>
+        {/* body */}
+        <p>
+          I'm a software developer that is aiming to become a FullStack Developer
+          by everyday trying to improve my skills and learnings in Web and Mobile development.
+          <br/><br/>
+          Actually I am focused in the Frontend area using libraries as React and React Native for work
+          but also studying on how to use others frameworks for Frontend like Angular.
+        </p>
+      </Terminal>
+      
+      <Terminal>
+        {/* header */}
+        <p>
+          <span style={{color: '#af5fff'}}>victorcruz</span> at 
+          <span style={{color: '#d75f00'}}> about</span> in 
+          <span style={{color: '#87fd03'}}> ~/portfolio</span> 
+          <span style={{color: '#56c2e6'}}> (main)</span><br/>
+          $ cd skills/ && ls
+        </p>
+        {/* body */}
+        {/* WIP */}
         <Box
-          borderRadius="0.5rem 0.5rem 0 0"
           display="flex"
-          alignItems="center"
-          p="0.5rem"
-          sx={{background: '#8c8c8c'}}
+          flexDirection="row"
+          justifyContent="space-evenly"
         >
-          <FiberManualRecord sx={{color: '#ff6057'}} fontSize="small" />
-          <FiberManualRecord sx={{color: '#ffbd2e'}} fontSize="small" />
-          <FiberManualRecord sx={{color: '#27c93f'}} fontSize="small" />
+          <Box>
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+            >
+              <img src={require('../../assets/icons/javascript.png')} />
+              <span>JavaScript</span>
+            </Box>
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+            >
+              <img src={require('../../assets/icons/typescript.png')}/>
+              <span>TypeScript</span>
+            </Box>
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+            >
+              <img src={require('../../assets/icons/php.png')}/>
+              <span>PHP</span>
+            </Box>
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+            >
+              <img src={require('../../assets/icons/laravel.png')} width={48} height={40}/>
+              <span>Laravel</span>
+            </Box>
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+            >
+              <img src={require('../../assets/icons/react.png')}/>
+              <span>React</span>
+            </Box>
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+            >
+              <img src={require('../../assets/icons/react.png')}/>
+              <span>React Native</span>
+            </Box>
+          </Box>
+          <Box>
+          <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+            >
+              <img src={require('../../assets/icons/node.png')} />
+              <span>Node</span>
+            </Box>
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+            >
+              <img src={require('../../assets/icons/git.png')}/>
+              <span>Git</span>
+            </Box>
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+            >
+              <img src={require('../../assets/icons/git.png')}/>
+              <span>Github</span>
+            </Box>
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+            >
+              <img src={require('../../assets/icons/gitlab.png')} />
+              <span>Gitlab</span>
+            </Box>
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+            >
+              <img src={require('../../assets/icons/postgre.png')}/>
+              <span>PostgreSQL</span>
+            </Box>
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+            >
+              <img src={require('../../assets/icons/mysql.png')}/>
+              <span>MySQL</span>
+            </Box>
+          </Box>
+          <Box>
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+            >
+              <img src={require('../../assets/icons/oracle.png')}/>
+              <span>OracleDB (PL/SQL)</span>
+            </Box>
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+            >
+              <img src={require('../../assets/icons/aws.png')}/>
+              <span>AWS Lambda</span>
+            </Box>
+          </Box>
         </Box>
-        <Box
-          borderRadius="0 0 0.5rem 0.5rem"
-          p="1rem"
-          fontFamily="Courier New, Courier, monospace"
-          fontSize="1.5rem"
-          color="#fff"
-          sx={{background: '#27242f'}}
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra nulla eget suscipit suscipit. Ut eu mi vel urna condimentum euismod a ut nisl. Praesent sagittis id leo sed tincidunt. Sed egestas augue eget dignissim tempor. Donec bibendum ornare ligula ut mattis. 
-        </Box>
-      </Box>
+        <br/>
+      </Terminal>
     </Box>
   );
 }
