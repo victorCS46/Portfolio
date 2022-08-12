c54bdd54cad8c9bab2d377b19ec3f27f8f69397e
Victor Cruz <vcruz@viva.com.do>
2022-08-12 10:15:17 -0400

    Setting project page ready

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { AxiosResponse } from 'axios';
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material';
+import { Box, Button, Card, CardActions, CardContent, CardMedia, CircularProgress, Typography, useTheme } from '@mui/material';
 import { getUserRepos } from '../../api/githubApi';
+import './projects.css';
+import { GitHub } from '@mui/icons-material';
 
 interface IRepo {
   name: string;
@@ -16,11 +18,12 @@ const username = 'victorCS46';
 const Projects = () => {
 
   const [repos, setRepos] = React.useState<IRepo[]>([]);
+  const { palette } = useTheme();
 
   React.useEffect(() => {
     getUserRepos(username)
       .then(({ data }: AxiosResponse) => {
-        const repo = data.map((item: any): IRepo => ({
+        const repo: IRepo[] = data.map((item: any): IRepo => ({
           name: item.name,
           description: item.description,
           url: item.html_url,
@@ -35,15 +38,17 @@ const Projects = () => {
   return (
     <React.Fragment>
       <Box
+        width={{xs: '60%', md: '80%'}}
+        margin="auto"
         display="flex"
         flexWrap="wrap"
-        gap={2}
-        justifyContent="space-around"
+        gap={3}
+        justifyContent="center"
       >
         {
           (repos.length !== 0)
           ? repos.map((item, index) => (
-            <Card key={ index } sx={{ maxWidth: 345 }} >
+            <Card key={ index } sx={{ width: 345 }} className="repo-shadow">
               <CardMedia
                 component="img"
                 height="225"
@@ -54,17 +59,33 @@ const Projects = () => {
                 <Typography gutterBottom variant="h5" component="div">
                   { item.name }
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant="body2" color={palette.text.secondary}>
                   { item.description }
                 </Typography>
               </CardContent>
               <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
+                <Button
+                  size="small"
+                  href={item.url}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Repository
+                </Button>
               </CardActions>
             </Card>
           )) : (
-            <></>
+            <Box
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <CircularProgress />
+              <Typography color={palette.primary.light}>
+                Requesting from github <GitHub fontSize="small" />
+              </Typography>
+            </Box>
           )
         }
       </Box>
